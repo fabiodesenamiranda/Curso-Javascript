c8ec4d3e6dd75d1f73543c0970b282d495f92e08
Fabio Sena Miranda <fabiodesenamiranda@hotmail.com>
2023-11-11 04:31:27 -0300

    atualizações_MVC

--- MVC-AlunosMedias-Inicio/js/Services/Alunos.service.js
+++ MVC-AlunosMedias-Inicio/js/Services/Alunos.service.js
@@ -1,6 +1,7 @@
 class AlunosService{
     constructor(){
         this.alunos = []
+        this.updateListAlunosFromLocalStorage()
     }
 
     add(aluno){
@@ -8,12 +9,33 @@ class AlunosService{
             throw TypeError("Aluno must be a instance of AlunoModel")
         }
         this.alunos.push(aluno)
+        this.updateLocalStorage()
         
     }    
 
     
     edit(aluno){
         return aluno
+    }
+
+    searchById(id){
+        return this.alunos.find(aluno => aluno._id === id)
+    }
+
+    updateLocalStorage(){
+        const alunos = JSON.stringify(this.alunos)
+        localStorage.setItem("alunos", alunos)
+    }
+
+    updateListAlunosFromLocalStorage(){
+        const local = localStorage.getItem("alunos")
+        if(local){
+            const alunos = JSON.parse(local)
+            alunos.forEach(aluno => {
+                this.add(new AlunoModel(aluno))
+            });
+
+        }
     }
 
 }
--- MVC-AlunosMedias-Inicio/js/app.js
+++ MVC-AlunosMedias-Inicio/js/app.js
@@ -1,42 +1,33 @@
 
-      const alunos = [
-        {
-          _id: 0,
-          nome: "chico melato",
-          notas: {
-            portugues: [1, 1, 2, 2],
-            matematica: [2, 2, 2, 2],
-            historia: [2, 2, 3, 3],
-            ciencias: [3, 3, 3, 3],
-          },
-        },
-        {
-          _id: 1,
-          nome: "talita lima",
-          notas: {
-            portugues: [4, 4, 4, 4],
-            matematica: [4, 4, 5, 5],
-            historia: [5, 5, 6, 6],
-            ciencias: [7, 7, 8, 9],
-          },
-        },
-      ];
+      // const alunos = [
+      //   {
+      //     _id: 0,
+      //     nome: "chico melato",
+      //     notas: {
+      //       portugues: [1, 1, 2, 2],
+      //       matematica: [2, 2, 2, 2],
+      //       historia: [2, 2, 3, 3],
+      //       ciencias: [3, 3, 3, 3],
+      //     },
+      //   },
+      //   {
+      //     _id: 1,
+      //     nome: "talita lima",
+      //     notas: {
+      //       portugues: [4, 4, 4, 4],
+      //       matematica: [4, 4, 5, 5],
+      //       historia: [5, 5, 6, 6],
+      //       ciencias: [7, 7, 8, 9],
+      //     },
+      //   },
+      // ];
 
 
 const alunosService = new AlunosService()
 
-// Calcula a media por materia de cada aluno e cria uma propriedade chamada media.
-
-alunos.forEach(aluno => {
-    alunosService.add(new AlunoModel(aluno))
-
-});
-
 const alunosView = new AlunosView(document.querySelector('[data-table-alunos]'))
 
-
 const alunosController = new AlunosController(alunosService, alunosView)
-
 
 document.querySelector("form").addEventListener("submit", function(e){
     e.preventDefault()
