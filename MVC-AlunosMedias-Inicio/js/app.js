d7d578ffcc9388bbae2e616505366e742b607b47
Fabio Sena Miranda <fabiodesenamiranda@hotmail.com>
2023-11-14 09:47:46 -0300

    atualização MVC

--- MVC-AlunosMedias-Inicio/js/app.js
+++ MVC-AlunosMedias-Inicio/js/app.js
@@ -1,31 +1,34 @@
 
-      // const alunos = [
-      //   {
-      //     _id: 0,
-      //     nome: "chico melato",
-      //     notas: {
-      //       portugues: [1, 1, 2, 2],
-      //       matematica: [2, 2, 2, 2],
-      //       historia: [2, 2, 3, 3],
-      //       ciencias: [3, 3, 3, 3],
-      //     },
-      //   },
-      //   {
-      //     _id: 1,
-      //     nome: "talita lima",
-      //     notas: {
-      //       portugues: [4, 4, 4, 4],
-      //       matematica: [4, 4, 5, 5],
-      //       historia: [5, 5, 6, 6],
-      //       ciencias: [7, 7, 8, 9],
-      //     },
-      //   },
-      // ];
+      const alunosOld = [
+        {
+          _id: 0,
+          nome: "chico melato",
+          notas: {
+            portugues: [1, 1, 2, 2],
+            matematica: [2, 2, 2, 2],
+            historia: [2, 2, 3, 3],
+            ciencias: [3, 3, 3, 3],
+          },
+        },
+        {
+          _id: 1,
+          nome: "talita lima",
+          notas: {
+            portugues: [4, 4, 4, 4],
+            matematica: [4, 4, 5, 5],
+            historia: [5, 5, 6, 6],
+            ciencias: [7, 7, 8, 9],
+          },
+        },
+      ];
 
 
 const alunosService = new AlunosService()
 
-const alunosView = new AlunosView(document.querySelector('[data-table-alunos]'))
+const alunosView = new AlunosView(
+  document.querySelector('[data-table-alunos]'),
+  new MateriasService().materias
+  )
 
 const alunosController = new AlunosController(alunosService, alunosView)
 
