0ffa68188ae0102bb0409b339267a1b9e32f37e0
Fabio Sena Miranda <fabiodesenamiranda@hotmail.com>
2023-11-27 09:27:54 -0300

    Common JS

---
+++ commonjs/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+const emoji = require("node-emoji")
+
+const mod1 = require("./modules/mod1")
+
+
+
+const mod2 = require("./modules/mod2")
+const mod3 = require("./modules/mod3")
+
+console.log(mod2)
+console.log(mod3)
+
+
+// fs.writeFile("teste.txt", "ola mundo", err => {
+//     if(err) throw err
+//     console.log("saved!", emoji.get("coffee"))
+// })
+
+
---
+++ commonjs/modules/mod1.js
@@ -0,0 +1,9 @@
+//const teste = "ola mundo"
+
+exports.fn = () =>{
+    console.log("função em module.exports")
+    return "--função em module.exports--"
+}
+
+
+// module.exports é o {} exportado
