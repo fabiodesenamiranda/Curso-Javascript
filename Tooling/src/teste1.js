61786b201e36fdaca25703a3e92bb7b854c43394
Fabio Sena Miranda <fabiodesenamiranda@hotmail.com>
2023-11-29 09:10:45 -0300

    atlz.webpack

--- Tooling/src/teste1.js
+++ Tooling/src/teste1.js
@@ -1,12 +1,20 @@
+import "core-js"
+import axios from "axios"
+
 
 const getAddress = async (cep) => {
     let url  = 'https://viacep.com.br/ws/${cep}/json/'
     try {
-        const resposta = await fetch(url)
-        if(!resposta.ok) throw Error ("invalid postal code")
-        const json = await resposta.json()
+        const resposta = await axios.get(url)
+       
+        const json = resposta.data
+        console.log(json)
         return json
     } catch (e) {
         throw e
     }  
 }
+
+console.log("---------")
+const endereco = getAddress("03136-050").then(data => console.log)
+console.log(endereco)
