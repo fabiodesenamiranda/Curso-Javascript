06e86ab8d21fef8c23d390529d794a3867c5cb68
Fabio Sena Miranda <fabiodesenamiranda@hotmail.com>
2023-12-15 11:15:17 -0300

    node_js

---
+++ nodejs/streams/writeStream.js
@@ -0,0 +1,17 @@
+const fs = require("fs")
+
+const ws = fs.createWriteStream("./file.md", "UTF-8")
+
+ws.write("# ola mundo")
+
+const readStream = fs.createReadStream("./swapi.json", "UTF-8")
+
+const writeStream = fs.createWriteStream("./swapi_clone.json","UTF-8")
+
+// readStream.on("data", data => {
+//     writeStream.write(data)
+// })
+
+readStream.pipe(writeStream).on("finish", () => {
+    console.log("finish")
+})
