a800986a3ee95b89c7ec0a3af9be73bf88e6b8c8
Fabio Sena Miranda <fabiodesenamiranda@hotmail.com>
2023-12-16 08:39:00 -0300

    node_server

--- nodejs/server/server.js
+++ nodejs/server/server.js
@@ -1,22 +1,40 @@
 const http = require("http")
 const fs = require("fs")
+const { url } = require("inspector")
 
 http.createServer((req, res) => {
     //res.writeHead(200, {"Content-Type": "text-plain"})
-    res.writeHead(200, {"Content-Type": "text/html"})
-
-fs.readFile("./index_original.html", (err, html) =>{
-    if(err){
-        res.end("Deu Erro")
-    }
-
-    const convertToTemplate = new Function("return `"+ html +"`")
-
-    res.end(convertToTemplate.call(req))
-})
+    
 
 
+    switch(req, url){
+        case "/":
+            case "index.html":
 
+               return fs.readFile("./index_original.html", (err, html) =>{
+                    if(err){
+                        res.writeHead(500)
+                        res.end("Deu Erro")
+                    }
+
+                    res.writeHead(200, {"Content-Type": "text/html"})
+                    const convertToTemplate = new Function("return `"+ html +"`")
+
+                    res.end(convertToTemplate.call(req))
+                    
+                })
+
+                case "/estilo/estilo.css":
+                    res.writeHead(200, {"Content-Type": "text/css"})
+                    return res.end("body {color: red; }")
+                case "/img/logo.img":
+                    res.writeHead(200, {"Content-Type": "image/jpg"})
+                   return fs.createReadStream("./image.jpg").pipe(res)
+                default:
+                    res.writeHead(404, {"Content-Type": "text/html"})
+                    res.end("<h1>404</h1>")
+
+        }
 }).listen(3000)
 
 console.log("servidor rodando na porta 3000 \n http://localhost:3000")
